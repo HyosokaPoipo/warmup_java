2152d79afbe80ea35a57560ac8096d28b6709219
Maryadi <maryadi@elephanttalk.com>
2016-06-03 11:10:13 +0700

    add frontBack method....

--- src/hisoka/poipo/MainClass.java
+++ src/hisoka/poipo/MainClass.java
@@ -4,7 +4,29 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//Reverse string
+		System.out.println(frontBack("Hellow Hyosoka"));
+		
+		//frontBack string
+		System.out.println(frontBack1("Hisoka"));
+		System.out.println(frontBack1(""));;
+	}
+	
+	
+	
+	public static String frontBack1(String str)
+	{
+		if (str.length()== 0 || str.length() == 1) return str;
+		char temp1 = str.charAt(0);
+		char temp2 = str.charAt(str.length()-1);
+		String temp3 = str.substring(1, str.length()-1);
+		String result = temp2+temp3+temp1;
+		return result;
+	}
+	
+	public static String frontBack(String input)
+	{
+		return new StringBuilder(input).reverse().toString();
 	}
 
 }
