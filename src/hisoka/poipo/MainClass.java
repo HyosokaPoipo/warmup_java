1fba55d70dd8330ee6cf142ea4fe254e38b10b7e
Maryadi <maryadi@elephanttalk.com>
2016-06-06 11:27:08 +0700

    count8

--- src/hisoka/poipo/MainClass.java
+++ src/hisoka/poipo/MainClass.java
@@ -38,9 +38,18 @@ public class MainClass {
 		System.out.println(bunnyEars2(2));
 		
 		System.out.println(sumDigits(12));
+		
+		System.out.println(count8(8818));
+		System.out.println(count8(8088));
 	}
 	
-	
+	public static int count8(int n) {
+		if(n<10 && n == 8) return 1; 
+		if(n<10 && n!=8) return 0;
+		if((n%10 == 8) && ((n/10)%10)==8) return 1+count8(n%10)+count8(n/10);
+		return count8(n%10)+count8(n/10);
+	}
+
 	
 	public static int count7(int n) {		  
 	  if(n<10 && n == 7) return 1;
